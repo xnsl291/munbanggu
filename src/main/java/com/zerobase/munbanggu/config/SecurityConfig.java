343ba1cad3eed27db404feaf6d30308612fd8c8f
jppark93 <pjp9157@gmail.com>
2023-11-17 12:11:52 +0900

    FEAT:POINT 충전 중간 커밋

--- src/main/java/com/zerobase/munbanggu/config/SecurityConfig.java
+++ src/main/java/com/zerobase/munbanggu/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -43,6 +44,11 @@ public class SecurityConfig {
                 .userInfoEndpoint().userService(customOAuth2UserService);
 
         return http.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
